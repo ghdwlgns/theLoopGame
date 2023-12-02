f0c7e67f671f238c63c59e0a01bcda39694b7d2a
ghdwlgns <atene1408@gmail.com>
2023-12-02 10:09:43 +0900

    임시 커밋

---
+++ object/Object.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+
+class Object:
+    def __init__(self, x, y, width, height, image_uri):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.object_sprite_sheet = pg.image.load(image_uri)
